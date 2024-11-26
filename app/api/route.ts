3d4f52e7c056dda2e1d94881fddbba5b32587b48
Anas <anascoder4@gmail.com>
2024-11-26 20:04:27 +0530

    initial commit

---
+++ app/api/route.ts
@@ -0,0 +1,29 @@
+import {GoogleGenerativeAI}  from "@google/generative-ai"
+
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY  as string)
+
+
+
+export async function  POST(request : Request) {
+    
+    try {
+        console.log(request)
+
+        const body = await request.json(); 
+        const responsedtext = await run(body.prompt)  ; 
+        return Response.json({data : responsedtext  , success : true})
+    } catch (error) {
+        return Response.json({success : false })
+    
+    }
+}
+
+
+
+async function run(prompt  : string) {
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+    return text ; 
+  }
--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,129 @@
+"use client"
+import React, { useState, useRef } from "react";
 import Image from "next/image";
+import {ReactTyped  ,Typed} from "react-typed";
+import { HashLoader } from "react-spinners";
 
-export default function Home() {
+function Page() {
+  const replaceNewlinesWithBr = (text: string) => {
+    return text.replace(/\n\s*\n/g, "<br><br>").replace(/\n/g, "<br>");
+  };
+
+  const typedRef = useRef<Typed | null>(null);
+  const [prompt, setPrompt] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [strings, setStrings] = useState([
+    "Ask me anything?",
+    "Write a Story About Batman",
+    "Write a JavaScript Program.",
+    "What is Gemini AI?",
+  ]);
+  const [useLoop, setUseLoop] = useState(true);
+  const [speed, setSpeed] = useState(60);
+
+  const fetchData = async (prompt: string) => {
+    if (loading) {
+      return;
+    }
+    setLoading(true);
+    const response = await fetch("/api", {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({ prompt }),
+    });
+    const data = await response.json();
+    console.log(data);
+
+    if (data.success) {
+      setLoading(false);
+      setPrompt("");
+      const promptString = replaceNewlinesWithBr(data.data);
+      setStrings([promptString]);
+    } else {
+      setStrings(["Something Went Wrong!"]);
+    }
+
+    setSpeed(10);
+    setUseLoop(false);
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPrompt(e.target.value);
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      fetchData(prompt);
+    }
+  };
+
+  const handleData = () => {
+    fetchData(prompt);
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="w-screen h-screen bg-zinc-900">
+      <div className="w-[90%] md:w-[70%] h-full mx-auto flex flex-col space-y-2">
+        <div className="logo w-full h-[10%] flex justify-center items-center space-x-4">
+          <Image
+            src="/logo.jpg"
+            width={50}
+            height={50}
+            alt="AlphaBot Logo"
+            className="rounded-full"
+          />
+          <div className="text-white font-bold text-2xl">AlphaBot</div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <div className="chatbox h-[75%] no-scrollbar overflow-auto">
+          {!loading && (
+            <ReactTyped
+              strings={strings}
+              typedRef={(typedInstance : any) => {
+                typedRef.current = typedInstance;
+              }}
+              typeSpeed={speed}
+              style={{ color: "white" }}
+              loop={useLoop}
+            />
+          )}
+
+          {loading && (
+            <div className="flex flex-col justify-center items-center h-full">
+              <h3 className="text-white font-bold my-2">Generating Response...</h3>
+              <HashLoader color="#bd51b2" />
+            </div>
+          )}
+        </div>
+
+        <div className="inputfield h-[10%]">
+          <div className="w-full flex items-center">
+            <input
+              type="text"
+              name="prompt"
+              id="prompt"
+              value={prompt}
+              onChange={handleChange}
+              className="w-[70%] md:w-[90%] py-2 px-2 rounded-l-lg bg-black border-2 box-border text-white border-r-0 focus:outline-none"
+              placeholder="Message AlphaBot"
+              onKeyDown={handleKeyDown}
+              style={{
+                paddingTop: "0.54rem",
+                paddingBottom: "0.55rem",
+              }}
+            />
+            <button
+              className="text-white py-3.5 bg-gradient-to-r from-purple-500 to-pink-500 w-[30%] md:w-[10%] focus:ring-4 focus:outline-none focus:ring-purple-200 font-medium rounded-r-lg text-sm px-5 text-center disabled:bg-black disabled:border"
+              onClick={handleData}
+              disabled={prompt.length === 0}
+            >
+              Send
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
+
+export default Page;
